3f5ca6c41a59d46cc01352c26a1f98cb9ff930f2
Christoph Mauel <s23cmaue@uni-bonn.de>
2023-06-29 18:29:54 +0200

    get_offers vollständig implementiert, Id System für Offers leicht optimiert

--- p3_mwe/extra/server.py
+++ p3_mwe/extra/server.py
@@ -15,7 +15,16 @@ async def welcome():
 
 @rest_api.get("/get_offers")
 async def get_offers():
-    return{"offers": "x"}
+    o=markt.get_offers()
+    out={}
+    for k,v in o.items():
+        a={ 'ID': k,
+            'Preis':v.get_preis(),
+            'Menge': v.get_anzahl(),
+            'Ware': v.get_warentyp()}
+
+        out[k]=a
+    return{"offers": out}
 
 @rest_api.get('/nutzer/{benutzername}')
 async def create_nutzer(benutzername: str, request: Request):
@@ -31,10 +40,30 @@ async def create_nutzer(benutzername: str, request: Request):
 async def get_nutzer_size():
     return {"user_size": markt.get_size_user()}
 
-@rest_api.get("/create_offer/{name}")
-async def create_offer(name: str, request: Request):
+
+@rest_api.get("/create_offer/{name}/{ware}/{menge}/{preis}")
+async def create_offer(name: str,ware: str, menge: int, preis: float, request: Request):
     pw=request.headers.get('pw')
     if markt.auth(name, pw):
+        paar = markt.create_offer(name,ware,menge,preis)
+        n = markt.get_user(name)
+        n.offer_einfügen(paar)
+        markt.edit_user(name,n)
         return{"status":"Angebot erfolgreich erstelt"}
     else:
         return{"status": "Benutzername oder Passwort falsch"}
+
+
+@rest_api.get('/size_my_offer/{name}')
+async def my_offer_size(name: str, request: Request):
+    pw = request.headers.get('pw')
+    if markt.auth(name, pw):
+        n = markt.get_user(name)
+        return {"my_offer_size" : n.get_my_offer_size()}
+    else:
+        return{"status": "Benutzername oder Passwort falsch"}
+
+
+@rest_api.get('/size_offer')
+async def get_nutzer_size():
+    return {"offer_size": markt.get_size_offers()}
