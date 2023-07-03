104dbb5f9a348bfb9e83a77d7594ae3590dc54e9
Imad <s47iaziz@uni-bonn.de>
2023-07-03 13:13:26 +0200

    main datei geslöscht (veraltet)

--- p3_mwe/build/CMakeFiles/handelsplatz.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for handelsplatz.
-# This may be replaced when dependencies are built.
--- p3_mwe/examples/demo_stockmarket.cpp
+++
@@ -1,9 +0,0 @@
-#include <pybind11/pybind11.h>
-
-namespace py = pybind11;
-
-
-PYBIND11_MODULE(stockmarket, m) {
-  m.doc() = "Stockmarket Example";
-  
-}
